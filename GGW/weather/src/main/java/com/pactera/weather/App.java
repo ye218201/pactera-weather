c941f54405b6078a68fef603d0efa733934e2be3
Yi Lun Ye <yeyylyyl@cn.ibm.com>
2019-06-24 22:24:49 +0800

    update app.java
    update app.java

--- GGW/weather/src/main/java/com/pactera/weather/App.java
+++ GGW/weather/src/main/java/com/pactera/weather/App.java
@@ -2,12 +2,14 @@ package com.pactera.weather;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 /**
  * Hello world!
  *
  */
 @SpringBootApplication
+@EnableWebMvc
 public class App {
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
